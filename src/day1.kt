cee4fc2a979d928dc289f4af6d109b938e5e496b
Prakhar Srivastav <prsrivastav@google.com>
2017-12-01 12:02:37 -0800

    added a functional approach

--- src/day1.kt
+++ src/day1.kt
@@ -3,9 +3,13 @@ import kotlin.test.assertEquals
 
 fun main(args: Array<String>) {
     val input = File("./input/day1.txt").readText()
+
     assertEquals(getSum(input), 1390) // part 1
-    val jump = input.length / 2
-    assertEquals(getSum(input, jump), 1232) // part 2
+    assertEquals(getSum(input, input.length/2), 1232) // part 2
+
+    // functional approach
+    assertEquals(getSumFunctional(input), 1390) // part 1
+    assertEquals(getSumFunctional(input, input.length/2), 1232) // part 2
 }
 
 fun getSum(s: String, jump: Int = 1): Int {
@@ -13,8 +17,16 @@ fun getSum(s: String, jump: Int = 1): Int {
     val n = s.length
     for (i in s.indices) {
         if (s[i] == s[(i + jump) % n]) {
-            total += s[i].toString().toInt()
+            total += s[i].toInt() - 48
         }
     }
     return total
+}
+
+fun getSumFunctional(s: String, jump: Int = 1): Int {
+    val n = s.length
+    return (0 until n)
+            .filter { s[it] == s[(it + jump) % n] }
+            .map { s[it].toInt() - 48 }
+            .sum()
 }
