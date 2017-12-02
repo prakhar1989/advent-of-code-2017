10e2f608148a6a7c585227f9a4c627c83988d5b7
Prakhar Srivastav <prsrivastav@google.com>
2017-12-01 21:57:08 -0800

    first attempt at day2

---
+++ src/day2.kt
@@ -0,0 +1,27 @@
+import java.io.File
+import kotlin.test.assertEquals
+
+fun main(args: Array<String>) {
+    val numbers = File("./input/day2.txt")
+            .readLines()
+            .map { it.split("\t") .map { it.toInt() } }
+
+    val part1 = numbers.map { it.max()!! - it.min()!! }.sum()
+    val part2 = numbers.map { findScore(it) }.sum()
+
+    assertEquals(part1, 37923)
+    assertEquals(part2, 263)
+}
+
+fun findScore(xs: List<Int>): Int {
+    for (i in 0 until xs.size) {
+        for (j in i+1 until xs.size) {
+            val a = xs[i]
+            val b = xs[j]
+            if (a % b == 0 || b % a == 0) {
+                return if (a > b) a / b else b / a
+            }
+        }
+    }
+    return 0
+}
